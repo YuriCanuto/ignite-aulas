ba25e51fd216889d889800ba0675815099ac00f3
Yuri Canuto <yuriasc@gmail.com>
2022-01-05 00:27:43 -0300

    Criação de componentes e uso do FlatList

--- react-native/myskills/src/pages/Home.js
+++ react-native/myskills/src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, TextInput, Platform, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Platform, FlatList, ScrollView } from 'react-native'
+import { Button } from '../components/Button'
+import { SkillCard } from '../components/SkillCard'
 
 export function Home() {
 
@@ -19,28 +21,20 @@ export function Home() {
             placeholderTextColor="#555"
             onChangeText={setNewSkill}
         />
-        <TouchableOpacity 
-            style={ styles.button }
-            activeOpacity={.7}
-            onPress={handleAddNewSkill}
-            >
-            <Text style={ styles.buttonText }>Add</Text>
-        </TouchableOpacity>
+
+        <Button onPress={handleAddNewSkill} />
+        
         <Text style={[ styles.title, { marginVertical: 25 } ]}>
             My Skills
         </Text>
 
-        {
-            mySkills.map(skill => (
-                <TouchableOpacity key={skill} style={styles.buttonSkill}>
-                    <Text style={styles.textSkill}>
-                        {skill}
-                    </Text>
-                </TouchableOpacity>
-            ))
-        }
-
-        
+        <FlatList 
+            data={mySkills}
+            keyExtractor={(item, index) => index}
+            renderItem={({ item }) => (
+                <SkillCard  skill={item} />
+            )}
+        />
         
     </View>
   )
@@ -65,29 +59,5 @@ const styles = StyleSheet.create({
         padding: Platform.OS === 'ios' ? 15 : 10,
         marginTop: 30,
         borderRadius: 7
-    },
-    button: {
-        backgroundColor: '#A370F7',
-        padding: 15,
-        borderRadius: 7,
-        alignItems: 'center',
-        marginTop: 15
-    },
-    buttonText: {
-        color: '#FFF',
-        fontSize: 17,
-        fontWeight: 'bold'
-    },
-    buttonSkill: {
-        backgroundColor: '#1F1E25',
-        padding: 15,
-        borderRadius: 50,
-        alignItems: 'center',
-        marginVertical: 10
-    },
-    textSkill: {
-        color: '#FFF',
-        fontSize: 22,
-        fontWeight: 'bold',
     }
 })
